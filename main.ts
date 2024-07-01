37523f6e84e6e6a22e2a3cb5473946e0d15a0cc6
ItIsIEli <eli.h.pope11@gmail.com>
2024-07-01 12:44:13 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,6 @@
-let checkPoint = 0
-let direction = 0
-let mySprite: Sprite = null
-let startTime = 0
-let bestTime = 0
-let lapTime = 0
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight0, function (sprite, location) {
     checkPoint = 1
 })
-function initTimeView () {
-	
-}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     music.playTone(392, music.beat(BeatFraction.Eighth))
     direction += 1
@@ -17,10 +8,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     setSpriteDirection()
 })
 function setSpriteDirection () {
-    let dy: number[] = []
-    let dx: number[] = []
-    let accel = 0
-    let cars: Image[] = []
     mySprite.setImage(cars[direction])
     mySprite.ax = accel * dx[direction]
     mySprite.ay = accel * dy[direction]
@@ -35,6 +22,45 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorMixed, function (spr
         }
     }
 })
+let startTime = 0
+let checkPoint = 0
+let cars: Image[] = []
+let dy: number[] = []
+let dx: number[] = []
+let direction = 0
+let accel = 0
+let mySprite: Sprite = null
+scene.setBackgroundColor(13)
+mySprite = sprites.create(sprites.vehicle.carRedLeft, SpriteKind.Player)
+mySprite.setPosition(192, 48)
+scene.cameraFollowSprite(mySprite)
+tiles.setTilemap(tilemap`level1`)
+accel = 220
+direction = 0
+dx = [
+-1,
+0,
+1,
+0
+]
+dy = [
+0,
+1,
+0,
+-1
+]
+cars = [
+sprites.vehicle.carRedLeft,
+sprites.vehicle.carRedFront,
+sprites.vehicle.carRedRight,
+sprites.vehicle.carRedBack
+]
+setSpriteDirection()
+let bestTime = 0
+let lapTime = 0
+checkPoint = 0
+game.showLongText("Turn your car by \"A\" button.", DialogLayout.Bottom)
+startTime = game.runtime()
 game.onUpdate(function () {
     mySprite.vx = mySprite.vx * 0.97
     mySprite.vy = mySprite.vy * 0.97
